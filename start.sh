fa9fc1fffa4d9485cf7723a00f3d8add3a1a6568
Akshay Chaturvedi <akshay.chaturvedi@frinks.ai>
2022-09-15 16:21:31 +0530

    modify env

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /workspace/tata-python-backend
-pm2-runtime "yarn dev"
+pm2 start "yarn dev" --name "backend"
 cd /workspace/tata-python-backend/scripts
 pm2-runtime "python3 clutch_plate.py"
 wait -n
